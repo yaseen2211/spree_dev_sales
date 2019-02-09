637e8c6d7155b8476019e208f58c9ad7fbbc6ec6
yaseen iqbal <yaseeniqbal119@gmail.com>
2019-02-09 20:15:26 +0500

    remove rails version

--- db/migrate/20140308215701_create_sale_prices.rb
+++ db/migrate/20140308215701_create_sale_prices.rb
@@ -1,4 +1,4 @@
-class CreateSalePrices < ActiveRecord::Migration[4.2]
+class CreateSalePrices < ActiveRecord::Migration
   def change
     create_table :spree_sale_prices do |t|
       t.integer :price_id
